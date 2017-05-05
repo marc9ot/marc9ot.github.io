6353630193bfb4d34a68d938c8cddc5782c648bf
Marc Ottenbacher <ottenbacher.marc@gmail.com>
2017-05-05 13:47:13 +0200

    Test

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-(function() {
-  var app = angular.module("PSPList", []);
+(function () {
+    var app = angular.module("PSPList", []);
 
-  var MainController = function($scope) {
+    var MainController = function ($scope, $http) {
 
-    $scope.headline = "Projektstrukturplan Liste";
+        $scope.headline = "Projektstrukturplan Liste";
+
+        var path = "https://secure-lowlands-23501.herokuapp.com/api";
+
+        // $scope.items = [{
+        //     'Titel': 'Speiseplan',
+        //     'Kategorie': 'Systemebene',
+        //     'Beschreibung': 'Im Speiseplan werden die Speisen',
+        //     'Aufwand': '5'
+        // }];
+
+        $http.get(path + "/ppsitems")
+            .then(function(response) {
+                $scope.items = response.data;
+            });
+
+        $scope.addRow = function (Titel, Kategorie, Beschreibung, Aufwand) {
+
+            $scope.items.push({
+                'Titel': $scope.Titel,
+                'Kategorie': $scope.Kategorie,
+                'Beschreibung': $scope.Beschreibung,
+                'Aufwand': $scope.Aufwand
+            });
+
+            $scope.Titel = "";
+            $scope.Kategorie = "";
+            $scope.Beschreibung = "";
+            $scope.Aufwand = "";
+        };
+
+    };
 
 
-    $scope.items = [{
-      'Titel': 'Speiseplan',
-      'Kategorie': 'Systemebene',
-      'Beschreibung': 'Im Speiseplan werden die Speisen',
-      'Aufwand': '5'
-    }];
-    $scope.addRow = function(Titel, Kategorie, Beschreibung, Aufwand) {
-  
-        $scope.items.push({
-        'Titel': $scope.Titel,
-        'Kategorie': $scope.Kategorie,
-        'Beschreibung': $scope.Beschreibung,
-        'Aufwand': $scope.Aufwand
-      });
-    
-      $scope.Titel= "";
-      $scope.Kategorie="";
-      $scope.Beschreibung="";
-      $scope.Aufwand="";
-};
-    
-  };
-
-
-
-  app.controller("MainController", ["$scope", MainController]);
+    app.controller("MainController", ["$scope", "$http", MainController]);
 }());
